9a3ada128515fbb83fcc93819238685f7d071a1b
Nitipat L <nitipat@igeargeek.com>
2021-04-13 15:14:22 +0700

    [Added] even or odd challenge

---
+++ learn/odd-even/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _, number := range numbers {
+		out := "odd"
+		if number%2 == 0 {
+			out = "even"
+		}
+		fmt.Println(number, "is", out)
+	}
+}
