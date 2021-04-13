0e5234b9488bce4e8bbf5b592e2691c1c660466f
Nitipat L <nitipat@igeargeek.com>
2021-04-13 15:00:31 +0700

    [Edited] add deck testing

--- learn/cards/main.go
+++ learn/cards/main.go
@@ -1,9 +1,10 @@
 package main
 
 func main() {
-	cards := newDeck()
-	// cards.print()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	// cards := newDeck()
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
+	cards.shuffle()
+	cards.print()
 }
