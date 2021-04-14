b69319a6f1b5a15f5eb791462dfbd106531edc3c
Nitipat L <nitipat@igeargeek.com>
2021-04-14 19:04:03 +0700

    [Added] go routine

---
+++ learn/channels/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://golang.org",
+		"https://amazon.com",
+		"https://stackoverflow.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(l string) {
+			time.Sleep(time.Second * 5)
+			checkLink(l, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is OK :)")
+	c <- link
+}
